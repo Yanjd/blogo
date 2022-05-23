f04865e9cef87676a337ff0e4641ea43c8756dcc
Yanjd <318008756@qq.com>
2022-05-23 23:24:56 +0800

    feat:jwt middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,11 +1,126 @@
 package middleware
 
-import "blogo/utils"
+import (
+	"blogo/utils"
+	"blogo/utils/errmsg"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
-var JwtKey = []byte(utils.JwtKey)
+type JWT struct {
+	JwtKey []byte
+}
 
-// 生成token
+func NewJWT() *JWT {
+	return &JWT{
+		[]byte(utils.JwtKey),
+	}
+}
 
-// 验证token
+type MyClaim struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+var (
+	TokenExpired   error = errors.New("token已过期，请重新登陆")
+	TokenNotValid  error = errors.New("token无效，请重新登陆")
+	TokenMalformed error = errors.New("token不正确，请重新登录")
+	TokenInvalid   error = errors.New("非法token，请重新登陆")
+)
+
+// CreateToken 生成token
+func (j *JWT) CreateToken(claim MyClaim) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString(j.JwtKey)
+}
+
+// ParseToken 验证token
+func (j *JWT) ParseToken(tokenString string) (*MyClaim, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &MyClaim{}, func(token *jwt.Token) (interface{}, error) {
+		return j.JwtKey, nil
+	})
+
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValid
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if token != nil {
+		if claims, ok := token.Claims.(*MyClaim); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+	}
+	return nil, TokenInvalid
+}
 
 // jwt中间件
+
+func JwtToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		tokenHeader := c.Request.Header.Get("Authorization")
+		if tokenHeader == "" {
+			code = errmsg.ErrTokenExist
+			c.JSON(http.StatusOK, gin.H{
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+
+		checkToken := strings.Split(tokenHeader, " ")
+		if len(checkToken) == 0 {
+			code = errmsg.ErrTokenFmtWrong
+			c.JSON(http.StatusOK, gin.H{
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+
+		if len(checkToken) != 2 || checkToken[0] != "Bearer" {
+			code = errmsg.ErrTokenFmtWrong
+			c.JSON(http.StatusOK, gin.H{
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
+			})
+		}
+
+		j := NewJWT()
+		claims, err := j.ParseToken(checkToken[1])
+		if err != nil {
+			if err == TokenExpired {
+				c.JSON(http.StatusOK, gin.H{
+					"status":  errmsg.ERROR,
+					"message": err.Error(),
+					"data":    nil,
+				})
+				c.Abort()
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{
+				"status":  errmsg.ERROR,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
+
+		c.Set("username", claims)
+		c.Next()
+	}
+}
